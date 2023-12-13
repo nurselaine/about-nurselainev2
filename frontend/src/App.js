1f107fffc87db8fa0ba99e2eb54239857b7bf1e5
nurselaine <Elaine.t.huynh@gmail.com>
2023-12-12 16:15:12 -0800

    Added basical scaffolding

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,9 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello World
-      </header>
-    </div>
+    <div className='App'></div>
   );
 }
 
