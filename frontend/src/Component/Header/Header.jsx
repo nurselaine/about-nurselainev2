105c5fc4a3013598bbda6924d7b4a4a125c6c7a8
nurselaine <Elaine.t.huynh@gmail.com>
2023-12-16 10:11:08 -0800

    Added responsive navbar with hamburger menu

--- frontend/src/Component/Header/Header.jsx
+++ frontend/src/Component/Header/Header.jsx
@@ -1,66 +1,151 @@
-import {useState, React} from "react";
+import { useState, React } from "react";
 import {
   useColorMode,
   Switch,
+  Box,
   Flex,
   Link,
   Button,
-  IconButton
+  Text,
+  IconButton,
 } from "@chakra-ui/react";
-import {HamburgerIcon, CloseIcon} from "@chakra-ui/icons";
-
+import { HamburgerIcon, CloseIcon, MoonIcon } from "@chakra-ui/icons";
 
 const Header = () => {
   const { colorMode, toggleColorMode } = useColorMode();
-  const isDark = colorMode === 'dark';
-  const [display, changeDisplay] = useState('none');
+  const isDark = colorMode === "dark";
+  const [display, changeDisplay] = useState("none");
+  const [menu, changeManu] = useState(false);
 
   return (
     <Flex>
-      <Flex 
-        position="fixed" 
-        top="1rem" 
-        right="1rem" 
-        align="center"
-      >
-
+      <Flex position="fixed" top="1rem" right="1rem" align="center">
         {/* Desktop */}
-        <Flex 
-          display={['none', "none", "flex", "flex"]}  
-        >
-          <Link href="/" passHref>
-            <Button as="a" variant="ghost" aria-label="Home" my={5} w="100%">
+        <Flex display={["none", "none", "flex", "flex"]}>
+          <Link href="/">
+            <Button
+              as="a"
+              variant="ghost"
+              aria-label="Home"
+              my={5}
+              w="100%"
+              color={isDark ? "gray.50" : "gray.800"}
+            >
               Home
             </Button>
           </Link>
 
-          <Link href="/about" passHref>
-            <Button as="a" variant="ghost" aria-label="About" my={5} w="100%">
+          <Link href="/about">
+            <Button
+              as="a"
+              variant="ghost"
+              aria-label="About"
+              my={5}
+              w="100%"
+              color={isDark ? "gray.50" : "gray.800"}
+            >
               About
             </Button>
           </Link>
 
-          <Link href="/contact" passHref>
-            <Button as="a" variant="ghost" aria-label="Contact" my={5} w="100%">
+          <Link href="/contact">
+            <Button
+              as="a"
+              variant="ghost"
+              aria-label="Contact"
+              my={5}
+              w="100%"
+              color={isDark ? "gray.50" : "gray.800"}
+            >
               Contact
             </Button>
           </Link>
         </Flex>
 
+        <Switch
+          color="green"
+          isChecked={isDark}
+          onChange={toggleColorMode}
+          mr={2}
+        />
         {/* Mobile */}
+
         <IconButton
           aria-label="Open Menu"
           size="lg"
-          mr={2}
-          icon={<HamburgerIcon />}
-          onClick={() => changeDisplay('flex')}
-          display={['flex', 'flex', 'none']}
+          icon={display === "flex" ? "" : <HamburgerIcon />}
+          bg={display === "flex" && 'none'}
+          onClick={() => changeDisplay("flex")}
+          display={["flex", "flex", "none", "none"]}
+          ml={"auto"}
         />
-        <Switch color="green" isChecked={isDark} onChange={toggleColorMode} />
       </Flex>
       {/* Mobile Content */}
+      <Flex
+        w="100vw"
+        h="100vh"
+        display={display}
+        zIndex={20}
+        pos="fixed"
+        top="0"
+        left="0"
+        overflowY="auto"
+        flexDir="column"
+      >
+        <Flex justify="flex-end">
+          <IconButton
+            mt={2}
+            mr={2}
+            aria-label="Open Menu"
+            size="lg"
+            icon={<CloseIcon />}
+            onClick={() => changeDisplay("none")}
+          />
+        </Flex>
+
+        <Flex flexDir="column" align="center">
+          <Link href="/">
+            <Button
+              as="a"
+              variant="ghost"
+              aria-label="Home"
+              my={5}
+              w="100%"
+              color={isDark ? "gray.50" : "gray.800"}
+            >
+              Home
+            </Button>
+          </Link>
+
+          <Link href="/about">
+            <Button
+              as="a"
+              variant="ghost"
+              aria-label="About"
+              my={5}
+              w="100%"
+              color={isDark ? "gray.50" : "gray.800"}
+            >
+              About
+            </Button>
+          </Link>
+
+          <Link href="/contact">
+            <Button
+              as="a"
+              variant="ghost"
+              aria-label="Contact"
+              my={5}
+              w="100%"
+              color={isDark ? "gray.50" : "gray.800"}
+            >
+              Contact
+            </Button>
+          </Link>
+        </Flex>
+      </Flex>
     </Flex>
-  )
-}
+  );
+};
 
 export default Header;
