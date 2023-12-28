6ed3933fd717532515fe2454de72d13764257000
nurselaine <Elaine.t.huynh@gmail.com>
2023-12-27 19:46:52 -0800

    Updated package.json with deployment url

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,47 +9,23 @@ const Home = React.lazy(() => import("./Page/Home/Home"));
 const Timeline = React.lazy(() => import("./Page/Timeline/Timeline"));
 const ErrorPage = React.lazy(() => import("./Page/Error/ErrorPage"));
 const ContactForm = React.lazy(() => import("./Page/ContactForm/ContactForm"));
-const Loading = React.lazy(() => import('./Page/Loading'));
+const Loading = React.lazy(() => import("./Page/Loading"));
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: (
-      <React.Suspense fallback={<Loading />}>
-        <Home />
-      </React.Suspense>
-    ),
-    errorElement: (
-      <React.Suspense fallback={<Loading />}>
-        <ErrorPage />
-      </React.Suspense>
-    ),
+    path: "./",
+    element: <Home />,
+    errorElement: <ErrorPage />
   },
   {
     path: "/timeline",
-    element: (
-      <React.Suspense fallback={<Loading />}>
-        <Timeline />
-      </React.Suspense>
-    ),
-    errorElement: (
-      <React.Suspense fallback={<Loading />}>
-        <ErrorPage />
-      </React.Suspense>
-    ),
+    element: <Timeline />,
+    errorElement: <ErrorPage />
   },
   {
     path: "/contactform",
-    element: (
-      <React.Suspense fallback={<Loading />}>
-        <ContactForm />
-      </React.Suspense>
-    ),
-    errorElement: (
-      <React.Suspense fallback={<Loading />}>
-        <ErrorPage />
-      </React.Suspense>
-    ),
+    element: <ContactForm />,
+    errorElement: <ErrorPage />
   },
 ]);
 
@@ -58,7 +34,9 @@ root.render(
   <React.StrictMode>
     <ChakraProvider>
       <AnimatePresence>
-        <RouterProvider router={router} />
+        <React.Suspense fallback={<Loading />}>
+          <RouterProvider router={router} />
+        </React.Suspense>
       </AnimatePresence>
     </ChakraProvider>
   </React.StrictMode>
